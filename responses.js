5471a4a8b3f9adfc1fc14ce9b1ae363c57cd8d73
anjahelfrich <58011113+anjahelfrich@users.noreply.github.com>
2022-09-11 20:50:59 -0600

    Page layout, navigation, folium map

---
+++ responses.js
@@ -0,0 +1,16 @@
+function burgerMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function homeMap() {
+  var map = L.map('map').setView([35.1072, -106.6166], 13)
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+}
