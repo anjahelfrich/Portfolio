98e06f452e06905c26266784bef86744f8b099dc
Anja Helfrich <anjahelfrich@gmail.com>
2022-11-20 08:24:11 -0700

    Start fixing hamburger menu functionality

--- responses.js
+++ responses.js
@@ -7,16 +7,6 @@ function burgerMenu() {
   }
 }
 
-function openBurgerMenu() {
-  document.getElementById("myTopnav").style.display = "block";
-  document.getElementById("myOverlay").style.display = "block";
-}
-
-function closeBurgerMenu() {
-  document.getElementById("myTopnav").style.display = "none";
-  document.getElementById("myOverlay").style.display = "none";
-}
-
 function homeMap() {
   var map = L.map('map').setView([35.1072, -106.6166], 13)
 
